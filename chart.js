135d2d6f4724edc06c90681712e2267c426ff4de
meganwhite <mfw247@gmail.com>
2018-01-08 18:18:36 -0500

    restyle page

--- chart.js
+++ chart.js
@@ -28,7 +28,9 @@ var line = d3.svg.line()
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
+    .attr("class", "chart")
+    .attr("id", "apple-stock-chart")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("data.tsv", function(error, data) {
